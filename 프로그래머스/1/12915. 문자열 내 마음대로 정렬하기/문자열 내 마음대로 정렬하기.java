f27c7242ce9070d2799b0eaaa24f59aa9dc1e8a8
Baegwa <160799011+KangBaekGwa@users.noreply.github.com>
2025-08-28 20:04:58 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.02 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (a, b) -> {
+            char c1 = a.charAt(n);
+            char c2 = b.charAt(n);
+            
+            if(c1 == c2) {
+                return a.compareTo(b);
+            }
+            
+            return Character.compare(c1, c2);
+        });
+        
+        return strings;
+    }
+}
