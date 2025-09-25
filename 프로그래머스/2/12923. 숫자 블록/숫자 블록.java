6cca3a881ca1480a5d66a8dd4c1ab0db8faa4f01
Baegwa <160799011+KangBaekGwa@users.noreply.github.com>
2025-09-25 22:11:38 +0900

    [level 2] Title: 숫자 블록, Time: 92.65 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.java
@@ -1,6 +1,4 @@
 // 각 자리수는 end 까지 중, 각자의 자신을 제외한 최대약수 아냐?
-// 10까지 있을때
-// 
 
 class Solution {
     private static final int LIMIT = 10_000_000;
@@ -21,15 +19,27 @@ class Solution {
         return result;
     }
 
+    // private int calc(int x) {
+    //     int r = (int) Math.sqrt(x);
+    //     for (int i = 2; i <= r; i++) {
+    //         if (x % i != 0) continue;
+    //         return x / i;
+    //     }
+    //     return 1;
+    // }
+    
     private int calc(int x) {
         int best = 1;
         int r = (int) Math.sqrt(x);
-
         for (int i = 2; i <= r; i++) {
             if (x % i != 0) continue;
             int pair = x / i;
-            if (pair <= LIMIT) return pair;
-            if (i <= LIMIT && i > best) best = i;
+            if (pair <= LIMIT) {
+                return pair;
+            }
+            if (i <= LIMIT && i > best) {
+                best = i;
+            }
         }
         return best;
     }
