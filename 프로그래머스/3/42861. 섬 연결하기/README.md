dccb90fa7eda11a4dd51792391efa2bfa9066eeb
baekgwa <160799011+KangBaekGwa@users.noreply.github.com>
2026-02-06 09:38:04 +0900

    [level 3] Title: 섬 연결하기, Time: 1.19 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 85.7 MB, 시간: 0.81 ms
+메모리: 85.2 MB, 시간: 1.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 03일 23:32:33
+2026년 02월 06일 09:38:01
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -13,44 +13,56 @@
 import java.util.*;
 
 class Solution {
+        class Edge {
+        int to;
+        int cost;
+
+        Edge(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
     public int solution(int n, int[][] costs) {
-        Arrays.sort(costs, (a, b) -> Integer.compare(a[2], b[2]));
-        
-        int[] parent = new int[n];
-        for(int i=0; i<n; i++) {
-            parent[i] = i;
+
+        List<Edge>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        
-        int totalCost = 0;
-        int edgeCount = 0;
-        
-        for(int[] cost : costs) {
-            int start = cost[0];
-            int end = cost[1];
-            int nowCost = cost[2];
-            
-            // 둘이 같은 집합이 아니면, 연결해버리기
-            if(find(parent, start) != find(parent, end)) {
-                union(parent, start, end);
-                edgeCount++;
-                totalCost += nowCost;
+
+        for (int[] c : costs) {
+            int from = c[0];
+            int to = c[1];
+            int cost = c[2];
+
+            graph[from].add(new Edge(to, cost));
+            graph[to].add(new Edge(from, cost));
+        }
+
+        boolean[] visited = new boolean[n];
+        PriorityQueue<Edge> pq = new PriorityQueue<>(
+                (a, b) -> a.cost - b.cost
+        );
+
+        pq.add(new Edge(0, 0));
+        int answer = 0;
+
+        while (!pq.isEmpty()) {
+            Edge cur = pq.poll();
+
+            if (visited[cur.to]) continue;
+
+            visited[cur.to] = true;
+            answer += cur.cost;
+
+            for (Edge next : graph[cur.to]) {
+                if (!visited[next.to]) {
+                    pq.add(next);
+                }
             }
-            
-            if(edgeCount == n-1) break;
         }
-        
-        return totalCost;
-    }
-    
-    private int find(int[] parent, int x) {
-        if(parent[x] == x) return x;
-        return parent[x] = find(parent, parent[x]);
-    }
-    
-    private void union(int[] parent, int start, int end) {
-        int rootStart = find(parent, start);
-        int rootEnd = find(parent, end);
-        parent[rootEnd] = rootStart;
+
+        return answer;
     }
 }
     
